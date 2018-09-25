2cd64625a2a934391a3665b7974c7601e0bf25bc
Salad <xa.lach@yahoo.com>
2018-09-26 00:56:04 +0700

    Add Cortana requisites function

--- S-a-l-a-d.psm1
+++ S-a-l-a-d.psm1
@@ -10,5 +10,21 @@ Function DisableRemoteDesktopNLA {
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "UserAuthentication" -Type DWord -Value 0
 }
 
+# Enable Cortana requisites
+Function EnableCortanaRequisites {
+    Write-Output "Enabling Cortana requisites..."
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name "DisableWindowsConsumerFeatures" -ErrorAction SilentlyContinue
+    Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -ErrorAction SilentlyContinue
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Type DWord -Value 1
+}
+
+# Disable Cortana requisites
+Function DisableCortanaRequisites {
+    Write-Output "Disabling Cortana requisites..."
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name "DisableWindowsConsumerFeatures" -Type DWord -Value 1
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type DWord -Value 0
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Type DWord -Value 0
+}
+
 # Export functions
 Export-ModuleMember -Function *
